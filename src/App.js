42910fb139d54725ea4ef6a2b3ca261526f8e5d4
ebmoutinho <elisabmoutinho@gmail.com>
2022-08-10 17:58:14 +0100

    Register Page done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div>
-      
+      <Register />
     </div>
   );
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { useRef } from 'react';
+
+function Register() {
+const name = useRef("");
+const email = useRef("");
+const password = useRef("");
+const dateOfBirth = useRef("");
+const address = useRef("");
+const phone = useRef("");
+const profilePhoto = useRef("");
+
+function handleRegisterNewUser(event) {
+    event.preventDefault();
+    console.log("User Registered with success!");
+}
+
+
+  return (
+    <>
+    <h2>Register new user</h2>
+        <form onSubmit={handleRegisterNewUser}>
+            <label>
+                <input type="text" name="name" ref={name} placeholder='Name' required />
+            </label>
+            <label>
+                <input type="email" name="email" ref={email} placeholder='Email' required />
+            </label>
+            <label>
+                <input type="password" name="password" ref={password} placeholder='Password' required />
+            </label>
+            <label>
+                <input type="date" name="dateOfBirth" ref={dateOfBirth} required />
+            </label>
+            <label>
+                <input type="text" name="address" ref={address} placeholder='Address' required />
+            </label>
+            <label>
+            <input type="tel" name="phone"
+                    pattern="[0-9]{3}[0-9]{3}[0-9]{3}"
+                    placeholder='Phone Number' minLength={9} maxLength={9}
+                    ref={phone} required />
+            </label>
+            <label>
+                {/* <input type="image" alt="Profile Image"
+                    src="/media/examples/login-button.png"></input> */}
+                <input type="url" name="url" id="url"
+                    placeholder="https://photo-example.com"
+                    pattern="https://.*" size="30"
+                    ref={profilePhoto} required>   
+                </input>
+            </label>
+            <button type='submit'>Register</button>
+        </form>
+        <p>
+            Already have an account?
+            <a href=""> Login here</a>
+        </p>
+    </>
+  )
+}
+
+export default Register
