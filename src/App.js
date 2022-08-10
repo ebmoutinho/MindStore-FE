8b58a5088b6f4ddcecfda3292d3eb42cb4bc1f62
ebmoutinho <elisabmoutinho@gmail.com>
2022-08-10 18:22:38 +0100

    Header component complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Header from './components/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Register />
       <Login />
     </div>
