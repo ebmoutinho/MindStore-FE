25544dcee6f7dc2835cffd4904f6017be0bc3b9e
ebmoutinho <elisabmoutinho@gmail.com>
2022-08-10 18:32:32 +0100

    elisa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,32 @@
-import React, { useRef } from 'react'
+import React, { useRef } from "react";
 //🛍
 
-
-
 function Header() {
-    const inputSearch = useRef("");
+	const inputSearch = useRef("");
 
-    
-    function handleEnterPress(event) {
-        if(event.key === 'Enter'){
-          console.log(inputSearch.current.value, 'enter press here! ')
-        }
-      }
+	function handleEnterPress(event) {
+		if (event.key === "Enter") {
+			console.log(inputSearch.current.value, "enter press here! ");
+		}
+	}
 
-
-  return (
-    <>
-        <header>
-            <nav>
-                <div className='logo-div'>Logo</div>
-                <div className='search-div'>
-                    <input type="text" placeholder='Search for a product' ref={inputSearch} onKeyPress={handleEnterPress} />
-                </div>
-                <div className='links-div'>
-                    <a href="#">Login</a>
-                    <a href="#">Register</a>
-                    <a href="#">Cart</a>
-                </div>
-            </nav>
-        </header>
-    </>
-  )
+	return (
+		<>
+			<header>
+				<nav>
+					<div className="logo-div">Logo</div>
+					<div className="search-div">
+						<input type="text" placeholder="Search for a product" ref={inputSearch} onKeyPress={handleEnterPress} />
+					</div>
+					<div className="links-div">
+						<a href="#">Login</a>
+						<a href="#">Register</a>
+						<a href="#">Cart</a>
+					</div>
+				</nav>
+			</header>
+		</>
+	);
 }
 
-export default Header
+export default Header;
