ccf76b080fdd2e1f114d5b7095372d610c4722cf
ebmoutinho <elisabmoutinho@gmail.com>
2022-08-11 11:05:47 +0100

    Header css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { Link } from "react-router-dom";
+import "./header.css";
 //🛍
 
 function Header() {
@@ -17,10 +18,11 @@ function Header() {
         <>
             <header>
                 <nav>
-                    <div className='logo-div'>Logo</div>
-                    <div className='search-div'>
-                        <input type="text" placeholder='Search for a product' ref={inputSearch} onKeyPress={handleEnterPress} />
-                    </div>
+                    <Link to="/" className="logo-div">Mind<span className="bold">Store</span></Link>
+                    {/* <p className='logo-div'>Logo</p> */}
+                    <label>
+                        <input className="search-div" type="text" placeholder='Search' ref={inputSearch} onKeyPress={handleEnterPress} />
+                    </label>
                     <div className='links-div'>
                         <Link to="/login">Login</Link>{/*url do site*/}
                         <Link to="/register">Register</Link>{/*url do site*/}
