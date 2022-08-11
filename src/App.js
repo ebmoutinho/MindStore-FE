dfeadbce1163928075205d11d5982f2a3ddb66db
ebmoutinho <elisabmoutinho@gmail.com>
2022-08-11 11:56:05 +0100

    js files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Home from './pages/Home';
-import Register from './pages/Register';
-import Login from './pages/Login';
+import Home from './pages/Home/Home';
+import Register from './pages/Register/Register';
+import Login from './pages/Login/Login';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
