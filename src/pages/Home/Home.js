56c16b5821e34335353a5f80e1058ff411dc5d0f
luism <luis.couto@mindswap.academy>
2022-08-11 19:44:28 +0100

    sort drop down menu

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-// import Sort from '../../components/Sort/Sort';
-import Teste from '../../components/Teste/Teste';
-
+import Sort from '../../components/Sort/Sort';
 
 function Home() {
     return (
         <>
             <Header />
-            {/* <Sort /> */}
-            <div>
-                <Teste />
-
-            </div>
+            <Sort />
             <Footer />
         </>
     )
