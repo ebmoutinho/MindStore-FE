5c7dbf3a7a40a95c05ee0ac6541e73b602243c88
luism <luis.couto@mindswap.academy>
2022-08-12 12:38:57 +0100

    home page - w/ css

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,52 @@
 import React from 'react';
+import heroImg from "../../assets/heroImg.jpg";
+import bannerImg from "../../assets/bannerImg.jpg";
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
+import Product from "../../components/Product/Product";
 import "./home.css";
 
 function Home() {
     return (
         <>
             <Header />
+            <div className="home-hero">
+                <div className="hero-body">
+                    <h1 className='hero-body-title'>On Sale <i>-50%</i></h1>
+                    <p className='hero-body-description'>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam soluta saepe et dolorem corporis numquam? Quaerat quod quae sapiente ullam magnam consequuntur recusandae? Iste enim reprehenderit voluptatem ex laudantium ut?
+                    </p>
+                    <button className="hero-body-button">View Products</button>
+                </div>
+
+                <img src={heroImg} alt="" className='hero-img' />
+            </div>
+
+            <div className="home-products">
+
+                <div className="home-product-gallery">
+                    <div className="home-product-border">
+                        <Product />
+                    </div>
+                    <div className="home-product-border">
+                        <Product />
+                    </div>
+                    <div className="home-product-border">
+                        <Product />
+                    </div>
+                    <div className="home-product-border">
+                        <Product />
+                    </div>
+                </div>
+            </div>
+
+            <div className="home-banner">
+                <div className="banner-body">
+                    <h2 className='banner-body-title'><i>Trending Now</i></h2>
+                    <button className="banner-body-button">View Products</button>
+                </div>
+                <img src={bannerImg} alt="" className="banner-img"/>
+            </div>
 
             <Footer />
         </>
