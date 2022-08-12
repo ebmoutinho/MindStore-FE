a37828377abe1a5c2e499143687a7d5ccdd13454
ebmoutinho <elisabmoutinho@gmail.com>
2022-08-12 13:23:26 +0100

    Price Range try

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,207 +1,228 @@
-import React from 'react'
-import { useState } from "react";
+import React from "react";
+import { useState, useRef } from "react";
 import "./filter.css";
 
 function Filter() {
-    const [isFilterClicked, setIsFilterClicked] = useState(false);
-    const [isCategoryClicked, setIsCategoryClicked] = useState(false);
-    const [isRatingClicked, setIsRatingClicked] = useState(false);
-    const [isPriceClicked, setIsPriceClicked] = useState(false);
+	const [isFilterClicked, setIsFilterClicked] = useState(false);
+	const [isCategoryClicked, setIsCategoryClicked] = useState(false);
+	const [isRatingClicked, setIsRatingClicked] = useState(false);
+	const [isPriceClicked, setIsPriceClicked] = useState(false);
+	const [range, setRange] = useState("");
+	const rangeRef = useRef("");
 
-    function FilterMainButton() {
-        // function handleFilterChange(event) {
-        //     console.log(event.target.value);
-        // };
-        function handleCategoryClick() {
-            setIsCategoryClicked(!isCategoryClicked);
-            console.log("category");
-        }
-        function handleRatingClick() {
-            setIsRatingClicked(!isRatingClicked);
-            console.log("rating");
-        }
-        function handlePriceClick() {
-            setIsPriceClicked(!isPriceClicked);
-            console.log("price");
-        }
+	function FilterMainButton() {
+		// function handleFilterChange(event) {
+		//     console.log(event.target.value);
+		// };
+		function handleCategoryClick() {
+			setIsCategoryClicked(!isCategoryClicked);
+			console.log("category");
+		}
+		function handleRatingClick() {
+			setIsRatingClicked(!isRatingClicked);
+			console.log("rating");
+		}
+		function handlePriceClick() {
+			setIsPriceClicked(!isPriceClicked);
+			console.log("price");
+		}
 
-        if(isFilterClicked) {
-            return( //form
-            <>
-                <form className='filter-form'>
-                    <label>
-                        <button name="filter" onClick={handleCategoryClick} value="category">Category</button>
-                    </label>
-                    <CategoryButton />
+		if (isFilterClicked) {
+			return (
+				//form
+				<>
+					<form className="filter-form">
+						<label>
+							<button name="filter" onClick={handleCategoryClick} value="category">
+								Category
+							</button>
+						</label>
+						<CategoryButton />
 
-                    <label>
-                       <button name="filter" onClick={handleRatingClick} value="rating">Rating</button>
-                    </label>
-                    <RatingButton />
+						<label>
+							<button name="filter" onClick={handleRatingClick} value="rating">
+								Rating
+							</button>
+						</label>
+						<RatingButton />
 
-                    <label>
-                      <button name="filter" onClick={handlePriceClick} value="price">Price</button>
-                    </label>
-                    <PriceButton />
-                </form>
-            </>
-            ) 
-        } else {
-            return;
-        }
-    }; //FilterMainButton
-    function handleFilterClick() {
-        setIsFilterClicked(!isFilterClicked);
-        setIsCategoryClicked(false); //para fechar as opcoes do botao quando abro e fecho o filter(principal)
-        setIsRatingClicked(false);
-        setIsPriceClicked(false);
-        console.log("filter");
-    };
+						<label>
+							<button name="filter" onClick={handlePriceClick} value="price">
+								Price
+							</button>
+						</label>
+						<PriceButton />
+					</form>
+				</>
+			);
+		} else {
+			return;
+		}
+	} //FilterMainButton
+	function handleFilterClick() {
+		setIsFilterClicked(!isFilterClicked);
+		setIsCategoryClicked(false); //para fechar as opcoes do botao quando abro e fecho o filter(principal)
+		setIsRatingClicked(false);
+		setIsPriceClicked(false);
+		console.log("filter");
+	}
 
-    function CategoryButton() {
-        function handleCategoryChange(event) {
-            console.log(event.target.value);
-        };
-        
-        if(isCategoryClicked) {
-            return (
-                <div>
-                    <form className='category-form'> 
-                        <label>
-                            <input type="radio" name='category' onChange={handleCategoryChange} value="women's clothing" />
-                            Women’s Clothing
-                        </label>
+	function CategoryButton() {
+		function handleCategoryChange(event) {
+			console.log(event.target.value);
+		}
 
-                        <label>
-                            <input type="radio" name='category' onChange={handleCategoryChange} value="men's clothing" />
-                            Men’s Clothing
-                        </label>
-                        
-                        <label>
-                            <input type="radio" name='category' onChange={handleCategoryChange} value="jewelery" />
-                            Jewellery
-                        </label>
-                                                
-                        <label>
-                            <input type="radio" name='category' onChange={handleCategoryChange} value="electronics" />
-                            Electronics
-                        </label>
-                    </form>
-                </div>
-            );
-        } else {
-            return;
-        }
-        
-    };
+		if (isCategoryClicked) {
+			return (
+				<div>
+					<form className="category-form">
+						<label>
+							<input type="radio" name="category" onChange={handleCategoryChange} value="women's clothing" />
+							Women’s Clothing
+						</label>
 
-    function RatingButton() {
-        function handleRatingChange(event) {
-            console.log(event.target.value);
-        };
+						<label>
+							<input type="radio" name="category" onChange={handleCategoryChange} value="men's clothing" />
+							Men’s Clothing
+						</label>
 
-        if(isRatingClicked) {
-            return(
-                <div>
-                    <form className='rating-form'>
-                        <label>
-                            <input type="radio" name="rating" onChange={handleRatingChange} value="rating from 0 to 1"/>
-                            0 - 1
-                        </label>
+						<label>
+							<input type="radio" name="category" onChange={handleCategoryChange} value="jewelery" />
+							Jewellery
+						</label>
 
-                        <label>
-                            <input type="radio" name="rating" onChange={handleRatingChange} value="rating from 1.1 to 2"/>
-                            1.1 - 2
-                        </label>
+						<label>
+							<input type="radio" name="category" onChange={handleCategoryChange} value="electronics" />
+							Electronics
+						</label>
+					</form>
+				</div>
+			);
+		} else {
+			return;
+		}
+	}
 
-                        <label>
-                            <input type="radio" name="rating" onChange={handleRatingChange} value="rating from 2.1 to 3"/>
-                            2.1 - 3
-                        </label>
+	function RatingButton() {
+		function handleRatingChange(event) {
+			console.log(event.target.value);
+		}
 
-                        <label>
-                            <input type="radio" name="rating" onChange={handleRatingChange} value="rating from 3.1 to 4"/>
-                            3.1 - 4
-                        </label>
+		if (isRatingClicked) {
+			return (
+				<div>
+					<form className="rating-form">
+						<label>
+							<input type="radio" name="rating" onChange={handleRatingChange} value="rating from 0 to 1" />0 - 1
+						</label>
 
-                        <label>
-                            <input type="radio" name="rating" onChange={handleRatingChange} value="rating from 4.1 to 5"/>
-                            4.1 - 5
-                        </label>
-                    </form>
-                </div>
-            );
-        } else {
-            return;
-        }
-        
-    };
+						<label>
+							<input type="radio" name="rating" onChange={handleRatingChange} value="rating from 1.1 to 2" />
+							1.1 - 2
+						</label>
 
-    function PriceButton() {
-        function handlePriceChange(event) {
-            console.log(event.target.value);
-        };
+						<label>
+							<input type="radio" name="rating" onChange={handleRatingChange} value="rating from 2.1 to 3" />
+							2.1 - 3
+						</label>
 
-        if(isPriceClicked) {
-            return (
-                <div>
-                    <form className='price-form'>
-                        <label>
-                            <input type="radio" name="price" onChange={handlePriceChange} value="price from 0 to 10"/>
-                            0€ to 10€
-                        </label>
+						<label>
+							<input type="radio" name="rating" onChange={handleRatingChange} value="rating from 3.1 to 4" />
+							3.1 - 4
+						</label>
 
-                        <label>
-                            <input type="radio" name="price" onChange={handlePriceChange} value="price from 11 to 20"/>
-                            11€ to 20€
-                        </label>
+						<label>
+							<input type="radio" name="rating" onChange={handleRatingChange} value="rating from 4.1 to 5" />
+							4.1 - 5
+						</label>
+					</form>
+				</div>
+			);
+		} else {
+			return;
+		}
+	}
 
-                        <label>
-                            <input type="radio" name="price" onChange={handlePriceChange} value="price from 21 to 50"/>
-                            21€ to 50€
-                        </label>
+	function PriceButton() {
+		function handlePriceChange(event) {
+			console.log(event.target.value);
+		}
 
-                        <label>
-                            <input type="radio" name="price" onChange={handlePriceChange} value="price from 51 to 100"/>
-                            51€ to 100€
-                        </label>
+		// function handleRangeChange(event) {
+		// 	setRange(event.target.value);
+		// 	console.log(event.target.value);
+		// 	// rangeRef.current = (event.target.value);
+		// }
 
-                        <label>
-                            <input type="radio" name="price" onChange={handlePriceChange} value="price from 51 to 100"/>
-                            51€ to 100€
-                        </label>
+		if (isPriceClicked) {
+			return (
+				<div>
+					<form className="price-form-range">
+						<label>
+							{/* <input type="range" value={range} min="1" max="100" onChange={handleRangeChange}/>
+                            <input type="range" value={range} min="1" max="100" step="2" onChange={handleRangeChange}/>
+                            <input type="range" min="1" max="100" onChange={handleRangeChange} />
+                            <input type="range" min="1" max="100" />
+                            <input type="range" min="1" max="100" onInput={handleRangeChange} />
+                            <input type="text" />
+                            <output>{range}</output> */}
+							<input type="range" min="0" max="5" value={range} onChange={(e) => setRange(e.target.value)} step="1" />
+                            {range}
+						</label>
+					</form>
 
-                        <label>
-                            <input type="radio" name="price" onChange={handlePriceChange} value="price from 101 to 500"/>
-                            101 to 500€
-                        </label>
+					<form className="price-form">
+						<label>
+							<input type="radio" name="price" onChange={handlePriceChange} value="price from 0 to 10" />
+							0€ to 10€
+						</label>
 
-                        <label>
-                            <input type="radio" name="price" onChange={handlePriceChange} value="more than 500"/>
-                            more than 500€
-                        </label>
-                    </form>
-                </div>
-            );
-        } else {
-            return;
-        }
-        
-    };
-    
+						<label>
+							<input type="radio" name="price" onChange={handlePriceChange} value="price from 11 to 20" />
+							11€ to 20€
+						</label>
 
-  return (
-    <>
-        <button onClick={handleFilterClick}>Filter by</button>
-        <FilterMainButton />
+						<label>
+							<input type="radio" name="price" onChange={handlePriceChange} value="price from 21 to 50" />
+							21€ to 50€
+						</label>
 
-    </>
-  )
+						<label>
+							<input type="radio" name="price" onChange={handlePriceChange} value="price from 51 to 100" />
+							51€ to 100€
+						</label>
+
+						<label>
+							<input type="radio" name="price" onChange={handlePriceChange} value="price from 51 to 100" />
+							51€ to 100€
+						</label>
+
+						<label>
+							<input type="radio" name="price" onChange={handlePriceChange} value="price from 101 to 500" />
+							101 to 500€
+						</label>
+
+						<label>
+							<input type="radio" name="price" onChange={handlePriceChange} value="more than 500" />
+							more than 500€
+						</label>
+					</form>
+				</div>
+			);
+		} else {
+			return;
+		}
+	}
+
+	return (
+		<>
+			<button onClick={handleFilterClick}>Filter by</button>
+			<FilterMainButton />
+		</>
+	);
 }
 
-export default Filter
-
-
+export default Filter;
 
 /*
     <>
