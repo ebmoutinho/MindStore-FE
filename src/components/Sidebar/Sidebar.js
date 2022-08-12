0e3ee52515dc94b54af085f84dd2cc4385fb9132
ebmoutinho <elisabmoutinho@gmail.com>
2022-08-12 10:29:02 +0100

    commit

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./sidebar.css"
+import Sort from '../Sort/Sort';
+
+function Sidebar() {
+  return (
+    <div className='filter'>
+      <Sort />
+    </div>
+  )
+}
+
+export default Sidebar;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
-// import Sort from '../../components/Sort/Sort';
-import Teste from '../../components/Teste/Teste';
+import Sidebar from '../../components/Sidebar/Sidebar';
 import Product from '../../components/Product/Product';
 import "./home.css";
 
@@ -10,9 +9,8 @@ function Home() {
     return (
         <>
             <Header />
-            {/* <Sort /> */}
             <div className='product-list-container'>
-                <Teste className="filter"/>
+                <Sidebar className="filter"/>
                 <div className='product-grid'>
                     <Product />
                     <Product />
