34530ac3cfbc25150f50bd9d60df7b76e42ed4e9
luism <luis.couto@mindswap.academy>
2022-08-13 16:03:34 +0100

    product detail page done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home';
 import Register from './pages/Credentials/Register';
 import Login from './pages/Credentials/Login';
 import ProductListPage from './pages/ProductListPage/ProductListPage';
+import ProductDetailPage from './pages/ProductDetailPage/ProductDetailPage';
 import Profile from './pages/Profile/Profile';
 import CartPage from "./pages/CartPage/CartPage"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -17,6 +18,7 @@ function App() {
           <Route path="register" element={<Register />} />
           <Route path="profile" element={<Profile />} />
           <Route path="productlistpage" element={<ProductListPage />} />
+          <Route path="productdetailpage" element={<ProductDetailPage />} />
           <Route path="cart" element={<CartPage />} />
         </Routes>
       </BrowserRouter>
