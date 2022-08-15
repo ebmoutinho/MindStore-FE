a8145326cd630167c560d3b5cf124f51169eff47
ebmoutinho <elisabmoutinho@gmail.com>
2022-08-15 17:58:42 +0100

    login fixed

--- src/pages/Credentials/Login.js
+++ src/pages/Credentials/Login.js
@@ -32,23 +32,26 @@ function Login() {
 			headers: { "Content-Type": "application/json" },
 			body: JSON.stringify({ email: email.current.value, password: password.current.value }),
 		};
-        
+
         try {
             const response = await fetch("/login", request);
-            // console.log("response", response.headers.get("Id"));
-            // console.log("response", response.headers.get("Authorization"));
-            // console.log("response", response.status);
-            setMessage("Login successful");
-            const givenToken = response.headers.get("Authorization");
-            setToken(givenToken); //Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJxdWltQGVtYWlsLmNvbSIsImV4cCI6MTY2MTQyNzQ1OH0.mvKcLrZqat8P2lrgyH765CSeCbQ9EefXpK2kVMfnKHSp7bj9yTy4jGlLRiWBKsuid9QJF0graoYuuZe9agfrCA
-            const givenId = response.headers.get("Id");
-            setId(givenId); //3
-            console.log("usertoken\n", givenToken);
-            console.log("userid\n", givenId);
-
-            localStorage.setItem('token', givenToken);
-            localStorage.setItem('Id', givenId);
-            localStorage.setItem('adminToken', "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6ZXRvQGVtYWlsLmNvbSIsImV4cCI6MTY2MTQyOTUyNH0.t9eLz6j70whrt17ea3DYrs7o96AVVKy93_EsS762IK9KjxVf-tCF2Cz1KPf0V1c-iOvbvQQy0fHvW25N-_Eb-Q");
+            if(response.status === 200) {
+                setMessage("Login successful");
+                const givenToken = response.headers.get("Authorization");
+                setToken(givenToken); //Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJxdWltQGVtYWlsLmNvbSIsImV4cCI6MTY2MTQyNzQ1OH0.mvKcLrZqat8P2lrgyH765CSeCbQ9EefXpK2kVMfnKHSp7bj9yTy4jGlLRiWBKsuid9QJF0graoYuuZe9agfrCA
+                const givenId = response.headers.get("Id");
+                setId(givenId); //3
+                console.log("usertoken\n", givenToken);
+                console.log("userid\n", givenId);
+                console.log(response.status);
+    
+                localStorage.setItem('token', givenToken);
+                localStorage.setItem('Id', givenId);
+                localStorage.setItem('adminToken', "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6ZXRvQGVtYWlsLmNvbSIsImV4cCI6MTY2MTQyOTUyNH0.t9eLz6j70whrt17ea3DYrs7o96AVVKy93_EsS762IK9KjxVf-tCF2Cz1KPf0V1c-iOvbvQQy0fHvW25N-_Eb-Q");
+            } else {
+                setMessage("credentials failed"); //devolve a msg OK ou ERRO do json login
+                setToken(null);
+            }
 
 
         } catch (e) {
@@ -58,42 +61,34 @@ function Login() {
     		setToken(null);
         }
 
-	
-        // const json = await response.json();
-        // console.log("user object: ", json);
-        // console.log(response.statusText);
-        // setStatus(response.status);
+        // try {
+        //     const response = await fetch("/login", request);
+        //     // console.log("response", response.headers.get("Id"));
+        //     // console.log("response", response.headers.get("Authorization"));
+        //     // console.log("response", response.status);
+        //     setMessage("Login successful");
+        //     const givenToken = response.headers.get("Authorization");
+        //     setToken(givenToken); //Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJxdWltQGVtYWlsLmNvbSIsImV4cCI6MTY2MTQyNzQ1OH0.mvKcLrZqat8P2lrgyH765CSeCbQ9EefXpK2kVMfnKHSp7bj9yTy4jGlLRiWBKsuid9QJF0graoYuuZe9agfrCA
+        //     const givenId = response.headers.get("Id");
+        //     setId(givenId); //3
+        //     console.log("usertoken\n", givenToken);
+        //     console.log("userid\n", givenId);
+        //     console.log(response.status);
 
-        // setMessage("login successful!"); //devolve a msg OK ou ERRO do json login
-        // setToken(json.token); //devolve o token que é um ESTADO, e podemos imprimir no HTML {token}
-        // localStorage.setItem("token", json.token);
-        // // console.log(json.token);
-        // // console.log(response.status); //200
+        //     localStorage.setItem('token', givenToken);
+        //     localStorage.setItem('Id', givenId);
+        //     localStorage.setItem('adminToken', "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ6ZXRvQGVtYWlsLmNvbSIsImV4cCI6MTY2MTQyOTUyNH0.t9eLz6j70whrt17ea3DYrs7o96AVVKy93_EsS762IK9KjxVf-tCF2Cz1KPf0V1c-iOvbvQQy0fHvW25N-_Eb-Q");
 
-        // if (response.status === 200) {
-		// 	setMessage("login successful!"); //devolve a msg OK ou ERRO do json login
-		// 	setToken(json.token); //devolve o token que é um ESTADO, e podemos imprimir no HTML {token}
-		// 	// localStorage.setItem("token", json.token);
-        //     console.log(json.token);
-        //     console.log(response.status); //200
-        // } else {
+
+        // } catch (e) {
+        //     console.log("error message ", e.message);
         //     console.log("login failed resposta martelada")
-		// 	setMessage("credentials failed"); //devolve a msg OK ou ERRO do json login
-		// 	setToken(null);
-		// }
+	    //     setMessage("credentials failed"); //devolve a msg OK ou ERRO do json login
+    	// 	setToken(null);
+        // }
 
-		// if (json.token === null) {
-		// 	setMessage("credentials failed"); //devolve a msg OK ou ERRO do json login
-		// 	setToken("");
-		// } else {
-		// 	setMessage("login successful!"); //devolve a msg OK ou ERRO do json login
-		// 	setToken(json.token); //devolve o token que é um ESTADO, e podemos imprimir no HTML {token}
-		// 	localStorage.setItem("token", json.token);
-		// }
 
-        // console.log("email: " + email.current.value);
-        // console.log("Password: " + password.current.value);
-    }
+    };
 
     return (
         <>
