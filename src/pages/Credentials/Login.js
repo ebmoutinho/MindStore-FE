e5802787faa25f29295c763567de8349262945ea
ebmoutinho <elisabmoutinho@gmail.com>
2022-08-15 12:37:55 +0100

    Login working

--- src/pages/Credentials/Login.js
+++ src/pages/Credentials/Login.js
@@ -8,13 +8,13 @@ import { useState } from 'react';
 
 //autoComplete='off'
 function Login() {
-    const username = useRef();
+    const email = useRef();
     const password = useRef();
     const [loginColor, setLoginColor] = useState(false);
 
 	const [message, setMessage] = useState("");
 	const [token, setToken] = useState("");
-    const [status, setStatus] = useState("");
+    const [id, setId] = useState("");
 
     useEffect(() => {
         setLoginColor(true);
@@ -24,37 +24,45 @@ function Login() {
     async function handleSubmit(event) {
         event.preventDefault();
 
-        // console.log(username, password);
+        // console.log(email, password);
 
 		const request = {
 			method: "POST",
 			headers: { "Content-Type": "application/json" },
-			body: JSON.stringify({ username: username.current.value, password: password.current.value }),
+			body: JSON.stringify({ email: email.current.value, password: password.current.value }),
 		};
         
         try {
-            const response = await fetch("https://fakestoreapi.com/auth/login", request);
-            const json = await response.json();
-            console.log("user object: ", json);
-            console.log(response.statusText);
-            setStatus(response.status);
-
-            setMessage("login successful!"); //devolve a msg OK ou ERRO do json login
-			setToken(json.token); //devolve o token que é um ESTADO, e podemos imprimir no HTML {token}
-			localStorage.setItem("token", json.token);
-            console.log(json.token);
-            console.log(response.status); //200
-
+            const response = await fetch("/login", request);
+            // console.log("response", response.headers.get("Id"));
+            // console.log("response", response.headers.get("Authorization"));
+            // console.log("response", response.status);
+            setMessage("Login successful");
+            const givenToken = response.headers.get("Authorization");
+            setToken(givenToken);
+            const givenId = response.headers.get("Id");
+            setId(givenId);
+            console.log(token);
+            console.log(id);
 
         } catch (e) {
             console.log("error message ", e.message);
             console.log("login failed resposta martelada")
 	        setMessage("credentials failed"); //devolve a msg OK ou ERRO do json login
     		setToken(null);
-            setStatus("401 martelado");
         }
 
 	
+        // const json = await response.json();
+        // console.log("user object: ", json);
+        // console.log(response.statusText);
+        // setStatus(response.status);
+
+        // setMessage("login successful!"); //devolve a msg OK ou ERRO do json login
+        // setToken(json.token); //devolve o token que é um ESTADO, e podemos imprimir no HTML {token}
+        // localStorage.setItem("token", json.token);
+        // // console.log(json.token);
+        // // console.log(response.status); //200
 
         // if (response.status === 200) {
 		// 	setMessage("login successful!"); //devolve a msg OK ou ERRO do json login
@@ -77,7 +85,7 @@ function Login() {
 		// 	localStorage.setItem("token", json.token);
 		// }
 
-        // console.log("Username: " + username.current.value);
+        // console.log("email: " + email.current.value);
         // console.log("Password: " + password.current.value);
     }
 
@@ -91,7 +99,7 @@ function Login() {
 
                 <form className='form' onSubmit={handleSubmit}>
                     <label className='label' htmlFor="login">
-                        <input autoFocus autoComplete="off" type="text" name="login" placeholder="Email" ref={username} required/>
+                        <input autoFocus autoComplete="off" type="text" name="login" placeholder="Email" ref={email} required/>
                     </label>
 
                     <label className='label' htmlFor="password">
@@ -99,8 +107,7 @@ function Login() {
                     </label>
                     <button className="button" type="submit">Login</button>
                 </form>
-                <p>Login status: {message}</p>
-                <p>Status: {status}</p>
+                <p>{message}</p>
                 <p className='footer-text'>
                     Don't have an account?
                     <Link to="/register" href="#"> Register here</Link>
