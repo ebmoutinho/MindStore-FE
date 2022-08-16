e94745714c55ff4812daa754ae6f68945f9e2bbe
luism <luis.couto@mindswap.academy>
2022-08-16 13:40:50 +0100

    rating in product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,18 +2,20 @@ import React from "react";
 import "./product.css";
 import { useState } from "react"
 import heartEmpty from "../../assets/heart-empty.png";
+import starFull from "../../assets/star-full.png";
 import { useEffect } from "react";
 
 function Product(props) {
 	const { productProp } = props;
 	const [productData, setProductData] = useState({});
+	const [rating, setRating] = useState();
 
-	useEffect( () => {
+	useEffect(() => {
 		async function fetchASingleProduct() {
 			const response = await fetch(`/api/v1/users/products/${productProp.id}`);
 			const json = await response.json();
-
 			setProductData(json);
+			setRating(json.rating);
 		};
 		fetchASingleProduct();
 	}, []);
@@ -31,7 +33,13 @@ function Product(props) {
 				</div>
 				<div className="product-description">
 					<p className="product-name">{productData.title}</p>
-					<p className="product-price">{productData.price}€</p>
+					<div>
+						<p className="product-rating">
+							{Math.round(rating?.rate * 10) / 10}
+							<img src={starFull} alt="" className=""/>
+						</p>
+						<p className="product-price">{productData.price}€</p>
+					</div>
 				</div>
 			</div>
 		</>
